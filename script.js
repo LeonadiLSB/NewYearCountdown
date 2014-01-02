35f2a560f8143ef50e7e9723ef054ffaeb1d25bc
Leon Bentrup <leon@leonadi.de>
2014-01-02 18:27:36 +0100

    Bugfixes, 2015

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var newYearDate = new Date("2014-01-01 00:00:00");
+var newYearDate = new Date("Jan 1, 2015 00:00:00");
 function addLeadingZeros(number, length) {
     var num = '' + number;
     while (num.length < length) num = '0' + num;
@@ -15,6 +15,7 @@ $(document).ready(function () {
             return;
         }
         $('#digits').show();
+		$('#currentTime').show();
         if(diff.getUTCDate() > 1) {
             $('#days').text(addLeadingZeros(diff.getUTCDate() - 1, 2));
         } else {
@@ -23,6 +24,9 @@ $(document).ready(function () {
         $('#hours').text(addLeadingZeros(diff.getUTCHours(), 2));
         $('#minutes').text(addLeadingZeros(diff.getUTCMinutes(), 2));
         $('#seconds').text(addLeadingZeros(diff.getUTCSeconds(), 2));
+		$('#shours').text(addLeadingZeros(currDate.getHours(), 2));
+		$('#sminutes').text(addLeadingZeros(currDate.getMinutes(), 2));
+		$('#sseconds').text(addLeadingZeros(currDate.getSeconds(), 2));
         if(diff <= 60000) {
             var barWidth = (1 - diff / 60000) * 100 + '%';
             $('#bar').animate({'width': barWidth}, 250);
